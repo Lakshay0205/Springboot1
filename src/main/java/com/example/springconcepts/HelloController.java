8ce2cdf60a9129cb86b415e1d99f3a39c1e3d626
Lakshay Kumar <lakshay.gla_cs21@gla.ac.in>
2025-03-02 20:24:23 +0530

    lakshay

---
+++ src/main/java/com/example/springconcepts/HelloController.java
@@ -0,0 +1,42 @@
+package com.example.springconcepts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController  // Marks this class as a Controller
+@RequestMapping("/api")  // Base URL path for endpoints
+class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz!";
+    }
+}
+
+
+
+
+@RestController  // Marks this class as a REST Controller
+@RequestMapping("/hello")  // Base URL path for endpoints
+class HelloControllerMark {
+
+    // Query Parameter API: localhost:8080/hello/query?name=Mark
+    @GetMapping("/query")
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
+
+@RestController  // Marks this class as a REST Controller
+@RequestMapping("/hello")  // Base URL path for endpoints
+class HelloController_PathVariable {
+
+    // Path Variable API: localhost:8080/hello/param/Mark
+    @GetMapping("/param/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz!";
+    }
+}
